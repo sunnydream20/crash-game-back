89cff4eba5780876842690041e907e7f064861ae
sunnydream20 <timonpoo220@gmail.com>
2024-09-04 05:17:55 +0300

    feat:broadcast

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /** @format */
 
-// /** @format */
 const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
@@ -30,7 +29,8 @@ app.use(cors());
 // Connect Database
 connectDB();
 
-//socket connection
+//store connecred users
+let usersBets = [];
 
 // require routers
 app.use("/api", require("./routers"));
@@ -38,19 +38,94 @@ app.use("/api", require("./routers"));
 const PORT = process.env.PORT || 5000;
 
 // crah
-let crashInterval;
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-
+let randomNum;
+let resultAmounts = {}; // Object to hold results for each user
 io.on("connection", async (socket) => {
-  console.log("a user connected", socket.id);
-
+  usersBets.push({ socketId: socket.id, crashAmount: 0, crashNumber: 1 });
   //Handle wslogin even
   loginhandler(io, socket);
   // socket.on("wsLogin", wslogin.wslogin);
+  socket.on("bet", (data) => {
+    const { crashAmount, crashNumber } = data;
+    const userBet = usersBets.find((user) => user.socketId === socket.id);
+    // if (crashNumber <= randomNum) {
+    //   resultAmount = crashAmount * crashNumber;
+    // } else {
+    //   resultAmount = -crashAmount;
+    // }
+    if (userBet) {
+      userBet.crashAmount = crashAmount;
+      userBet.crashNumber = crashNumber;
+    }
+  });
 });
+
+const startEmittingNumbers = () => {
+  randomNum = getRandomNumber();
+  countUpToNumber(randomNum, io);
+};
+
+const countUpToNumber = (targetNumber, io) => {
+  currentNumber = 1.0; // Start counting from 1.00
+
+  intervalId = setInterval(() => {
+    // Check if we reached or exceeded the target
+    currentNumber = parseFloat((currentNumber + 0.01).toFixed(2)); // Increment by 0.01
+
+    io.emit("crashingnumber", currentNumber);
+    if (currentNumber >= targetNumber) {
+      io.emit("loading", { message: "Loading..." });
+
+      usersBets.forEach((user) => {
+        if (user.crashNumber <= randomNum) {
+          resultAmounts[user.socketId] = user.crashAmount * user.crashNumber;
+        } else {
+          resultAmounts[user.socketId] = -user.crashAmount;
+        }
+      });
+      for (const user of usersBets) {
+        io.to(user.socketId).emit("result", {
+          resultAmount: resultAmounts[user.socketId] || 0,
+        });
+      }
+      // io.emit("result", resultAmount);
+      resultAmounts = {};
+      clearInterval(intervalId); // Stop the timer
+      // wait for 3 seconds
+
+      setTimeout(() => {
+        startEmittingNumbers();
+      }, 10000);
+    }
+  }, 200); // 100 ms interval for each increment
+};
+
+const getRandomNumber = () => {
+  const randomValue = Math.random();
+
+  let number;
+  if (randomValue < 0.9) {
+    // 90% chance for the range [1, 2]
+    number = 1 + Math.random(); // Generates a float between 1 and 2
+  } else if (randomValue < 0.99) {
+    // 9% chance for the range [2.1, 3.9]
+    number = 2.1 + Math.random() * (3.9 - 2.1); // Generates a float between 2 and 10
+  } else if (randomValue < 0.999) {
+    // 0.9% chance for the range [4, 5.9]
+    number = 4 + Math.random() * (5.9 - 4); // Generates a float between 11 and 50
+  } else {
+    // 0.1% chance for the range [6, 10]
+    number = 6 + Math.random() * (10 - 6); // Generates a float between 51 and 100
+  }
+
+  return parseFloat(number.toFixed(2)); // To return it with two decimal places
+};
+
+startEmittingNumbers();
 
 io.listen(8000);
--- socket/loginhandler.js
+++ socket/loginhandler.js
@@ -19,9 +19,6 @@ module.exports = (io, socket) => {
     socket.emit("loginSuccess", result.email);
   });
 
-  let isActive = false; // Flag to track if the game is active
-  let intervalId;
-
   socket.on("enterGame", async () => {
     // emit confirm message which of user entereted sucessfully!
     socket.emit("enterSucess");
@@ -31,66 +28,18 @@ module.exports = (io, socket) => {
 
     // start the process
     isActive = true;
-
-    setTimeout(() => {
-      startEmittingNumbers();
-    }, 5000);
   });
 
   socket.on("leaveGame", () => {
-    clearInterval(intervalId);
-    isActive = false;
     socket.emit("leaveSucess");
   });
+  socket.on("crash", (data) => {
+    const { crashAmount, crashNumber } = data;
+    const winningAmount = crashAmount * crashNumber;
+    socket.emit("crashingRes", winningAmount);
+  });
 
-  const startEmittingNumbers = () => {
-    countUpToNumber(getRandomNumber());
-  };
-  // socket.on("leaveGame", async () => {
-  //   socket.emit("leaveSucess");
-  // });
-
-  const getRandomNumber = () => {
-    const randomValue = Math.random();
-
-    let number;
-    if (randomValue < 0.9) {
-      // 90% chance for the range [1, 2]
-      number = 1 + Math.random(); // Generates a float between 1 and 2
-    } else if (randomValue < 0.99) {
-      // 9% chance for the range [2, 10]
-      number = 2 + Math.random() * (10 - 2); // Generates a float between 2 and 10
-    } else if (randomValue < 0.999) {
-      // 0.9% chance for the range [11, 50]
-      number = 11 + Math.random() * (50 - 11); // Generates a float between 11 and 50
-    } else {
-      // 0.1% chance for the range [51, 100]
-      number = 51 + Math.random() * (100 - 51); // Generates a float between 51 and 100
-    }
-
-    return parseFloat(number.toFixed(2)); // To return it with two decimal places
-  };
-
-  const countUpToNumber = (targetNumber) => {
-    let currentNumber = 1.0; // Start counting from 1.00
-
-    intervalId = setInterval(() => {
-      console.log(currentNumber.toFixed(2));
-      // Check if we reached or exceeded the target
-      currentNumber = parseFloat((currentNumber + 0.01).toFixed(2)); // Increment by 0.01
-
-      socket.emit("crashingnumber", currentNumber);
-      if (currentNumber >= targetNumber) {
-        socket.emit("loading", { message: "Loading..." });
-
-        clearInterval(intervalId); // Stop the timer
-        // wait for 3 seconds
-
-        setTimeout(() => {
-          startEmittingNumbers();
-        }, 5000);
-        console.log(`Reached the target number: ${targetNumber.toFixed(2)}`);
-      }
-    }, 200); // 100 ms interval for each increment
-  };
+  socket.on("bet", (data) => {
+    const { crashAmount, crashNumber } = data;
+  });
 };
